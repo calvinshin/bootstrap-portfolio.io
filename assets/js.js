25a39195a4b69bd06973c81f6bd9da521c1b4647
Calvin Shin <calvin.shin@gmail.com>
2019-03-22 23:59:13 -0700

    V1 completion; all content/structures have been added that were in scope for original project. Added :hover elements in css and updated click(function() to modify classes on the page.

--- assets/js.js
+++ assets/js.js
@@ -4,4 +4,5 @@ window.addEventListener("hashchange", function() { scrollBy(0, -50) })
 $(".nav-link").click(function() {
     $(".nav-link").removeClass("active");
     $(this).addClass("active");
+    $("div.show").removeClass("show");
   });
